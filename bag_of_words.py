bebb51db6ee593da7db3f38442f2aac02044151e
Michael Walczyk <michael.walczyk@obscuradigital.com>
2017-10-09 16:26:13 -0800

    Working on understanding skip-gram model.

--- bag_of_words.py
+++ bag_of_words.py
@@ -8,6 +8,7 @@ import requests
 import io
 from zipfile import ZipFile
 from tensorflow.contrib import learn
+from pprint import pprint
 
 # Start a graph session
 sess = tf.Session()
@@ -64,21 +65,20 @@ text_lengths = [len(x.split()) for x in texts]
 text_lengths = [x for x in text_lengths if x < 50]
 plt.hist(text_lengths, bins=25)
 plt.title('Histogram of # of Words in Texts')
-plt.show()
-
-# Choose max text word length at 25
-sentence_size = 25
-min_word_freq = 3
+#plt.show()
 
 # Setup vocabulary processor
+sentence_size = 25
+min_word_freq = 3
 vocab_processor = learn.preprocessing.VocabularyProcessor(sentence_size, min_frequency=min_word_freq)
 
 # Have to fit transform to get length of unique words
 vocab_processor.transform(texts)
 embedding_size = len([x for x in vocab_processor.transform(texts)])
+print('Embedding size:', embedding_size)
 
 # Split up data set into train / test
-train_indices = np.random.choice(len(texts), round(len(texts)*0.8), replace=False)
+train_indices = np.random.choice(len(texts), round(len(texts) * 0.8), replace=False)
 test_indices = np.array(list(set(range(len(texts))) - set(train_indices)))
 texts_train = [x for ix, x in enumerate(texts) if ix in train_indices]
 texts_test = [x for ix, x in enumerate(texts) if ix in test_indices]
@@ -103,3 +103,36 @@ x_col_sums = tf.reduce_sum(x_embed, 0)
 
 # Perform logistic regression
 x_col_sums_2d = tf.expand_dims(x_col_sums, 0)
+model_output = tf.add(tf.matmul(x_col_sums_2d, A), b)
+
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=model_output, labels=y_target))
+pred = tf.sigmoid(model_output)
+opt = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+train_step = opt.minimize(loss)
+
+sess.run(tf.global_variables_initializer())
+
+losses = []
+train_acc_all = []
+train_acc_avg = []
+
+for ix, t in enumerate(vocab_processor.fit_transform(texts_train)):
+    y_data = [[target_train[ix]]]
+    sess.run(train_step, feed_dict={x_data: t, y_target: y_data})
+
+    temp_loss = sess.run(loss, feed_dict={x_data: t, y_target: y_data})
+    losses.append(temp_loss)
+
+    if (ix + 1) % 10 == 0:
+        print('Training observation #{}: Loss = {}'.format(ix + 1, temp_loss))
+
+    # Keep a trailing average of the past 50 observations accuracy
+
+    [[temp_pred]] = sess.run(pred, feed_dict={x_data: t, y_target: y_data})
+
+    # Get true / false if prediction is accurate
+    train_acc_temp = target_train[ix] == np.round(temp_pred)
+    train_acc_all.append(train_acc_temp)
+    if len(train_acc_all) >= 50:
+        train_acc_avg.append(np.mean(train_acc_all[-50:]))
+        
